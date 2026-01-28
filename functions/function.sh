cbbcffcf89a8f8f8155f53f1e0475ea06163ea1c
Ahmed Salman <haameed.salman@gmail.com>
2026-01-28 10:16:19 +0000

    functions

---
+++ functions/function.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+#Method 1: with function keyword
+function greet_user() {
+    echo "Hello there!"
+}
+
+#Method 2: without function keyword
+greet_user2() {
+    echo "Hello World!"
+}
+
+#Calling BOTH functions
+greet_user  #Output: Hello there!
+greet_user2 #Output: Hello World!
